64646d2b9b7f1cb90579f4992a5b9b993344f783
hujinhai <hjhandxjlno.1@126.com>
2017-02-17 15:04:25 +0800

    1

--- distributed-transaction-test-dubboservice1/distributed-transaction-test-dubboservice1-provider/src/main/java/com/distributed/transaction/test/dubboservice1/provider/TestTransactionAgentService1Impl.java
+++ distributed-transaction-test-dubboservice1/distributed-transaction-test-dubboservice1-provider/src/main/java/com/distributed/transaction/test/dubboservice1/provider/TestTransactionAgentService1Impl.java
@@ -15,24 +15,25 @@ public class TestTransactionAgentService1Impl implements DubboWithDistributedTra
 
 	public CommonResponse service1() {
 		System.out.println("execute=================================== service1 ===================");
-		StackTraceElement[] stack = (new Throwable()).getStackTrace();
-		System.out.println("编号	类名                                      方法名            行数");
-		String str = " ";
-		for (int i = 0; i < stack.length; i++) {
-			StackTraceElement ste = stack[i];
-			// System.out.println(ste.getClassName() + "." + ste.getMethodName()
-			// + "(...);");
-			// System.out.println(i + "--" + ste.getMethodName());
-			if (i == 0) {
-				for (int j = 0; j < (89 - ste.getClassName().length()); j++) {
-					str = str + " ";
-				}
-			}
-			System.out.println(i + "	" + ste.getClassName() + str + ste.getMethodName() + "	" + ste.getLineNumber());
-			// System.out.println(i + "--" + ste.getLineNumber());
-			str = " ";
-		}
-		return new CommonResponse();
+//		StackTraceElement[] stack = (new Throwable()).getStackTrace();
+//		System.out.println("编号	类名                                      方法名            行数");
+//		String str = " ";
+//		for (int i = 0; i < stack.length; i++) {
+//			StackTraceElement ste = stack[i];
+//			// System.out.println(ste.getClassName() + "." + ste.getMethodName()
+//			// + "(...);");
+//			// System.out.println(i + "--" + ste.getMethodName());
+//			if (i == 0) {
+//				for (int j = 0; j < (89 - ste.getClassName().length()); j++) {
+//					str = str + " ";
+//				}
+//			}
+//			System.out.println(i + "	" + ste.getClassName() + str + ste.getMethodName() + "	" + ste.getLineNumber());
+//			// System.out.println(i + "--" + ste.getLineNumber());
+//			str = " ";
+//		}
+		throw new RuntimeException();
+		//return new CommonResponse();
 
 	}
 
